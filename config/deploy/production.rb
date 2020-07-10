53eec6a5778f67001c0abe100a92a07b0a71e52e
HilotoTaketake <hirotomacbook@gmail.com>
2020-07-10 11:51:00 +0900

    environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.249.55.86,' user: 'ec2-user', roles: %w{app db web}
+server '54.249.55.86', user: 'ec2-user', roles: %w{app db web}
